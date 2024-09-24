22b5823798081b8bc18fef24b528e45b94ad7c6b
FriedlA22021 <FriedlA22021@sus.htl-grieskirchen.at>
2024-09-24 11:10:14 +0200

    In Klasse HalloJavamitForEach List angelegt und mit forEach schleife ausgegeben

---
+++ Lamda_hausuebung/src/net/htlgkr/friedla22021/lamda_hausuebung/HalloJavamitForEach.java
@@ -0,0 +1,16 @@
+package net.htlgkr.friedla22021.lamda_hausuebung;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+
+
+    public static void main(String[] args) {
+        List<String> forEachList = new ArrayList<>();
+        for (String string : forEachList) {
+            System.out.println(string);
+        }
+
+    }
+}
